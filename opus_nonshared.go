867541549ca5f8b4db2b92fd1dded8711256a27d
Tim Cooper <tim.cooper@layeh.com>
2016-06-02 16:18:21 -0300

    link with math library ("m") on freebsd when using nonshared
    updates #6

--- opus_nonshared.go
+++ opus_nonshared.go
@@ -2,7 +2,7 @@
 
 package gopus
 
-// #cgo linux darwin LDFLAGS: -lm
+// #cgo linux darwin freebsd LDFLAGS: -lm
 //
 // #cgo CFLAGS: -Iopus-1.1.2/include -Iopus-1.1.2/celt -Iopus-1.1.2/silk -Iopus-1.1.2/silk/float -Iopus-1.1.2/silk/fixed
 //
